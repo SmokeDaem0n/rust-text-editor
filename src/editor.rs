6fa2a76d26ceaef63433324eda9ee657e2fa466e
SmokeDaem0n <odominguez.lehman@gmail.com>
2026-07-28 12:43:34 -0400

    separated code into editor + main file

---
+++ src/editor.rs
@@ -0,0 +1,37 @@
+use ::std::io;
+use crossterm::terminal::disable_raw_mode;
+use crossterm::terminal::enable_raw_mode;
+use std::io::Read;
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn default() -> Editor {
+        Editor {}
+    }
+
+    pub fn run(&self) {
+        enable_raw_mode().unwrap();
+        for b in io::stdin().bytes() {
+            match b {
+                Ok(b) => {
+                    let c = b as char;
+                    if c.is_control() {
+                        println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+                    } else {
+                        println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+                    }
+
+                    if c == 'q' {
+                        disable_raw_mode().unwrap();
+                        break;
+                    }
+                }
+                Err(_) => {
+                    println!("Error")
+                }
+            }
+        }
+        disable_raw_mode().unwrap();
+    }
+}
